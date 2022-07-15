aebc6a86965a029c44b005c782942b5502c46f6b
andrea1893 <andrea_li1893@yeah.net>
2022-07-15 22:32:43 +0800

    添加系统用户

--- shell_zh/adduser_zh.sh
+++ shell_zh/adduser_zh.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-# adduser--Adds a new user to the system, including building their
-# home directory, copying in default config data, etc.
-# For a standard Unix/Linux system.
+# 添加用户，定义其家目录，拷贝默认配置文件，设置密码等
+# 适用于linux/Unix系统
 pass_file="/etc/passwd" 
 shadow_file="/etc/shadow"
 group_file="/etc/group"
 homedir="/home"
 
 if [ "$(id -un)" != "root" ] ; then
- echo "Error: You must be root to run this command." >&2
+ echo "警告: 需要管理员权限！" >&2
  exit 1
 fi
 
-echo "Add new user account to $(hostname)"
+echo "正在向 $(hostname) 添加新用户"
 echo -n "login: " ; read login
-# The next line sets the highest possible user ID value at 5000,
-# but you should adjust this number to match the top end 
-# of your user ID range.
+# 定义uid值不超过5000, 并且按照现有用户中最大的uid的开始递增创建用户的uid
 uid="$(awk -F: '{ if (big < $3 && $3 < 5000) big=$3 } END { print big + 1 }' $pass_file)"
 homedir=$homedir/$login
 
-# give each user their own group.
+# 增加家目录
 gid=$uid
-echo -n "full name: " ; read fullname
+echo -n "fullname: " ; read fullname
 echo -n "shell: " ; read shell
 echo "Setting up account $login for $fullname..."
 echo ${login}:x:${uid}:${gid}:${fullname}:${homedir}:$shell >> $pass_file
@@ -32,5 +29,5 @@ mkdir $homedir
 cp -R /etc/skel/.[a-zA-Z]* $homedir
 chmod 755 $homedir
 chown -R ${login}:${login} $homedir
-# Setting an initial password
+# 定义初始密码
 exec passwd $login
